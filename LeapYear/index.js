fca7bfecfb086d98e741f7f2db7de0b775986c5a
Andrey <andrey@pop-os.localdomain>
2021-03-18 18:52:52 +0200

    create function which count type of the year

---
+++ LeapYear/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let year = +prompt("What is the year?");
+
+function leapYear(year) {
+    if (isFinite(year)) {
+        (year % 400 && ((year % 4) && (year % 100)))
+            ? alert("Not leap year") : alert("Leap year");
+    }
+}
