690df76744e7bc094bb1df3b136aa49d7a9aa31d
Andrey Bandura <andretan322@gmail.com>
2021-03-18 17:17:30 +0200

    add check for the same number

--- ThreeDigitNumber/index.js
+++ ThreeDigitNumber/index.js
@@ -4,6 +4,11 @@ let number = prompt("Type the number");
 
 function numberDigits(number) {
     if (isFinite(number) && number.length <= 3) {
-        alert("Hello");
+        (number[0] == number[1] || number[1] == number[2] ||
+            number[0] == number[2]) ? alert("true") : alert("false");
+    }   else {
+            alert("Please, type the correct number");
     }
 }
+
+numberDigits(number);
